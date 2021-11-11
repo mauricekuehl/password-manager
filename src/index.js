687129c568c0649e925dd9220a9c469b709a0526
maurice kuehl <maurice.kuehl@protonmail.com>
2021-11-11 16:29:46 +0100

    added to api signin,get all data, add record, delete record

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const crypto = require("crypto");
+const { json } = require("express");
 
 const PORT = 3000;
 app.listen(PORT, () => {
@@ -9,3 +10,67 @@ app.listen(PORT, () => {
 });
 app.use(express.static("public"));
 app.use(express.json());
+
+app.get("/api", (req, res) => {
+  const key = hash(req.body.auth);
+  const path = `database/${key}.json`;
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      res.json(JSON.parse(data));
+    });
+  } else {
+    res.sendStatus(409);
+  }
+});
+
+app.post("/api/signup", (req, res) => {
+  const key = hash(req.body.auth);
+  const path = `database/${key}.json`;
+  if (fs.existsSync(path)) {
+    res.sendStatus(409).send();
+    console.log("allready existing");
+  } else {
+    fs.writeFile(path, JSON.stringify({ key: key }), "utf8", (err, data) => {
+      console.error(err, data);
+    });
+    res.sendStatus(200);
+  }
+});
+
+app.post("/api", (req, res) => {
+  const key = hash(req.body.auth);
+  const path = `database/${key}.json`;
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      let fileData = JSON.parse(data);
+      fileData[req.body.name] = req.body.content;
+      fs.writeFile(path, JSON.stringify(fileData), "utf8", (err, data) => {
+        if (err) console.error(err, data);
+        res.status(200).send();
+      });
+    });
+  } else {
+    res.status(409).send();
+  }
+});
+
+app.delete("/api", (req, res) => {
+  const key = hash(req.body.auth);
+  const path = `database/${key}.json`;
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      let fileData = JSON.parse(data);
+      delete fileData[req.body.name];
+      fs.writeFile(path, JSON.stringify(fileData), "utf8", (err, data) => {
+        if (err) console.error(err, data);
+        res.status(200).send();
+      });
+    });
+  } else {
+    res.status(409).send();
+  }
+});
+
+function hash(input) {
+  return crypto.createHash("sha512").update(`${input}`).digest("hex");
+}
