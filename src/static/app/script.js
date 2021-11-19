2402fbda25c241a3da71e22f533179db8144bb80
maurice kuehl <maurice.kuehl@protonmail.com>
2021-11-19 18:58:39 +0100

    added change/view modal of records

--- src/static/app/script.js
+++ src/static/app/script.js
@@ -30,35 +30,59 @@ function renderData() {
   let html = "";
   for (const key in data) {
     if (Object.hasOwnProperty.call(data, key)) {
-      const element = data[key];
-      if (element.url === "") {
+      const elm = data[key];
+      if (elm.url === "") {
         var hostname = "";
       } else {
-        var hostname = new URL(element.url).hostname;
+        var hostname = new URL(elm.url).hostname;
       }
       html += `<div class="record">
-      <button class="copyToClipboard" data-value="${element.username}" >copy username</button>
-      <button class="copyToClipboard" data-value="${element.password}" >copy password</button>
-      <a href="${element.url}" target="_blank" rel=""><img src="" alt="go to url" data-value="${element.url}" /></a>
-      <img
-      height="64"
-      width="64"
-      src="https://icons.duckduckgo.com/ip3/${hostname}.ico"
-      />
+        <img data-value="${elm.name}" class="logo"
+          height="64"
+          width="64"
+          src="https://icons.duckduckgo.com/ip3/${hostname}.ico"
+        />
+        <button class="copyToClipboard" data-value="${elm.username}" >copy username</button>
+        <button class="copyToClipboard" data-value="${elm.password}" >copy password</button>
+        <a href="${elm.url}" target="_blank" rel="">
+          <img src="" alt="go to url" data-value="${elm.url}" />
+        </a>
+        <p>${elm.name}</p>
       </div>`;
     }
   }
   //I am aware that this is vulnerable to XSS, but there are bigger problems in case someone gets access to your passwords.
   document.querySelector("#entries").innerHTML = html;
+  //  document.querySelectorAll("[data-modal-target]").click;
 }
+document.querySelectorAll(".record .logo").forEach((img) => {
+  img.addEventListener("click", (elm) => {
+    const form = document.querySelector("#addRecordForm");
+    const data = JSON.parse(sessionStorage.getItem("data"))[
+      elm.srcElement.dataset.value
+    ];
+    for (const key in data) {
+      if (Object.hasOwnProperty.call(data, key)) {
+        form[key].value = data[key];
+      }
+    }
+    document.querySelector("#openModal").click();
+  });
+});
 document.querySelectorAll(".copyToClipboard").forEach((button) => {
   button.addEventListener("click", (elm) => {
-    // console.log(elm.srcElement.dataset.value);
     navigator.clipboard.writeText(elm.srcElement.dataset.value);
   });
 });
+function clearForm(form) {
+  form.name.value = "";
+  form.username.value = "";
+  form.password.value = "";
+  form.url.value = "";
+  form.totp.value = "";
+  form.notes.value = "";
+}
 document.querySelector("#addRecordForm").addEventListener("submit", (form) => {
-  const username = form.srcElement.username.value;
   const data = {
     name: form.srcElement.name.value,
     username: form.srcElement.username.value,
@@ -113,4 +137,5 @@ function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove("active");
   overlay.classList.remove("active");
+  clearForm(document.querySelector("#addRecordForm"));
 }
